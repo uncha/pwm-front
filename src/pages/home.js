27ea8e8dbeb227741647c2428b91cc37ad848a53
kyutaekim <>
2022-03-20 20:23:28 +0900

    fetch

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import { Box, Grid, makeStyles } from "@material-ui/core";
+import axios from "axios";
 import IframeResizer from "iframe-resizer-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,6 +12,12 @@ const useStyles = makeStyles((theme) => ({
 
 function Home() {
   const classes = useStyles();
+
+  useEffect(()=>{
+    fetch('http://54.166.12.79:3000/api/test').then(res=>{
+      console.log('res @@@', res)
+    })
+  }, [])
 
   return (
     <div className={classes.container}>
